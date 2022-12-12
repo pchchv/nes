7e243074dfc3f1a6825925714aacebca0798720d
pchchv <ipchchv@gmail.com>
2022-12-12 13:50:33 +0300

    0xAA opcode implementation and test

--- src/cpu.rs
+++ src/cpu.rs
@@ -2,6 +2,7 @@ pub struct CPU {
     pub register_a: u8,
     pub status: u8,
     pub program_counter: u16,
+    pub register_x: u8,
 }
 
 impl CPU {
@@ -10,6 +11,7 @@ impl CPU {
             register_a: 0,
             status: 0,
             program_counter: 0,
+            register_x: 0,
         }
     }
 
@@ -41,6 +43,21 @@ impl CPU {
                 0x00 => {
                     return;
                 }
+                0xAA => {
+                    self.register_x = self.register_a;
+
+                    if self.register_x == 0 {
+                        self.status = self.status | 0b0000_0010;
+                    } else {
+                        self.status = self.status & 0b1111_1101;
+                    }
+
+                    if self.register_x & 0b1000_0000 != 0 {
+                        self.status = self.status | 0b1000_0000;
+                    } else {
+                        self.status = self.status & 0b0111_1111;
+                    }
+                }
                 _ => todo!(),
             }
         }
@@ -66,5 +83,14 @@ mod test {
         let mut cpu = CPU::new();
         cpu.interpret(vec![0xa9, 0x00, 0x00]);
         assert!(cpu.status & 0b0000_0010 == 0b10);
+    }
+
+    #[test]
+    fn test_0xaa_tax_move_a_to_x() {
+        let mut cpu = CPU::new();
+        cpu.register_a = 10;
+        cpu.interpret(vec![0xaa, 0x00]);
+
+        assert_eq!(cpu.register_x, 10)
     }
 }
