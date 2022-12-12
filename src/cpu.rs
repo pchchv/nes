7232a44b8dfe25c4d25acbd3e37e44ed02271fae
pchchv <ipchchv@gmail.com>
2022-12-12 15:03:39 +0300

    An implementation using some of Rust's bit arithmetic capabilities

--- src/cpu.rs
+++ src/cpu.rs
@@ -64,6 +64,19 @@ impl CPU {
         self.program_counter = 0x8000;
     }
 
+    fn mem_read_u16(&mut self, pos: u16) -> u16 {
+        let lo = self.mem_read(pos) as u16;
+        let hi = self.mem_read(pos + 1) as u16;
+        (hi << 8) | (lo as u16)
+    }
+
+    fn mem_write_u16(&mut self, pos: u16, data: u16) {
+        let hi = (data >> 8) as u8;
+        let lo = (data & 0xff) as u8;
+        self.mem_write(pos, lo);
+        self.mem_write(pos + 1, hi);
+    }
+
     pub fn run(&mut self) {
         loop {
             let opscode = self.mem_read(self.program_counter);
