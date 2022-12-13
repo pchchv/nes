dd36323480e44b4b3005bc622ffa68c0147a15d4
pchchv <ipchchv@gmail.com>
2022-12-13 13:28:34 +0300

    Update run func

--- src/cpu.rs
+++ src/cpu.rs
@@ -582,6 +582,13 @@ impl CPU {
     }
 
     pub fn run(&mut self) {
+        self.run_with_callback(|_| {});
+    }
+
+    pub fn run_with_callback<F>(&mut self, mut callback: F)
+    where
+        F: FnMut(&mut CPU),
+    {
         let ref opcodes: HashMap<u8, &'static opcodes::OpCode> = *opcodes::OPCODES_MAP;
 
         loop {
@@ -589,9 +596,7 @@ impl CPU {
             self.program_counter += 1;
             let program_counter_state = self.program_counter;
 
-            let opcode = opcodes
-                .get(&code)
-                .expect(&format!("OpCode {:x} is not recognized", code));
+            let opcode = opcodes.get(&code).unwrap();
 
             match code {
                 0xa9 | 0xa5 | 0xb5 | 0xad | 0xbd | 0xb9 | 0xa1 | 0xb1 => {
@@ -753,6 +758,7 @@ impl CPU {
                 /* JSR */
                 0x20 => {
                     self.stack_push_u16(self.program_counter + 2 - 1);
+
                     let target_address = self.mem_read_u16(self.program_counter);
 
                     self.program_counter = target_address
@@ -880,6 +886,8 @@ impl CPU {
             if program_counter_state == self.program_counter {
                 self.program_counter += (opcode.len - 1) as u16;
             }
+
+            callback(self);
         }
     }
 }
