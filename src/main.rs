d9602b902c243ae23b23bbd76a399c6ca1ac4a3f
pchchv <ipchchv@gmail.com>
2022-12-13 13:07:11 +0300

    Implement handle_user_input func

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,10 @@ pub mod opcodes;
 
 use cpu::Mem;
 use cpu::CPU;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
+use sdl2::EventPump;
 
 #[macro_use]
 extern crate lazy_static;
@@ -37,6 +40,43 @@ fn read_screen_state(cpu: &CPU, frame: &mut [u8; 32 * 3 * 32]) -> bool {
         frame_idx += 3;
     }
     update
+}
+
+fn handle_user_input(cpu: &mut CPU, event_pump: &mut EventPump) {
+    for event in event_pump.poll_iter() {
+        match event {
+            Event::Quit { .. }
+            | Event::KeyDown {
+                keycode: Some(Keycode::Escape),
+                ..
+            } => std::process::exit(0),
+            Event::KeyDown {
+                keycode: Some(Keycode::W),
+                ..
+            } => {
+                cpu.mem_write(0xff, 0x77);
+            }
+            Event::KeyDown {
+                keycode: Some(Keycode::S),
+                ..
+            } => {
+                cpu.mem_write(0xff, 0x73);
+            }
+            Event::KeyDown {
+                keycode: Some(Keycode::A),
+                ..
+            } => {
+                cpu.mem_write(0xff, 0x61);
+            }
+            Event::KeyDown {
+                keycode: Some(Keycode::D),
+                ..
+            } => {
+                cpu.mem_write(0xff, 0x64);
+            }
+            _ => { /* do nothing */ }
+        }
+    }
 }
 
 fn main() {
