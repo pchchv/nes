35d491d1fb3f782115dceb87a5e0add008f64990
pchchv <ipchchv@gmail.com>
2022-12-13 16:50:14 +0300

    Implement registers for ppu

---
+++ src/ppu/mod.rs
@@ -0,0 +1,8 @@
+use crate::cartridge::Mirroring;
+use registers::addr::AddrRegister;
+use registers::control::ControlRegister;
+use registers::mask::MaskRegister;
+use registers::scroll::ScrollRegister;
+use registers::status::StatusRegister;
+
+pub mod registers;
