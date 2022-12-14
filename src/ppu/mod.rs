9450ca546fd4cdc0a1d97e9e923888a1ed68f612
pchchv <ipchchv@gmail.com>
2022-12-14 15:02:30 +0300

    Change Bus working with PPU

--- src/ppu/mod.rs
+++ src/ppu/mod.rs
@@ -116,7 +116,7 @@ impl NesPPU {
         return false;
     }
 
-    fn poll_nmi_interrupt(&mut self) -> Option<u8> {
+    pub fn poll_nmi_interrupt(&mut self) -> Option<u8> {
         self.nmi_interrupt.take()
     }
 }
